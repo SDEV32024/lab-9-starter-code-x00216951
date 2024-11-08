516b8edcd1569f7cd249da894c32a5ffd1e40af8
x00216951 <x00216951@mytudublin.ie>
2024-11-08 15:49:10 +0000

    Lab-9 part 1-2 commit

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
 # Create your models here.
 
 class CustomUser(AbstractUser):
     age = models.PositiveBigIntegerField(null=True, blank = True)
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        get_user_model(),
+        null=True,
+        on_delete=models.CASCADE,
+    )
+
+    date_of_birth = models.DateField(blank=False, null=False)
+    fav_author = models.CharField(max_length=255)
+
+    def __str__(self):
+        return str(self.user)
+    
+
+    def get_absolute_url(self):
+        return reverse('show_profile', args=[str(self.id)])
