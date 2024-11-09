68649ec625f5faae87d64082254ea903f4b60aa7
x00216951 <x00216951@mytudublin.ie>
2024-11-09 16:31:42 +0000

    lab 9 part7 commit

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,61 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Profile
 
-# Create your tests here.
+class CustomUserTestCase(TestCase):
+    
+    def setUp(self):
+    # Create a custom user for testing
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            email='testuser@t.com',
+            password='tpassword',
+            age=23
+        )
+    
+    def test_create_user(self):
+        # Test if a user is created properly
+        self.assertEqual(self.user.username, 'testuser')
+        self.assertEqual(self.user.email, 'testuser@t.com')
+        self.assertEqual(self.user.age,23)
+        self.assertTrue(self.user.check_password('tpassword'))
+
+    def test_superuser_creation(self):
+        # Test creating a superuser
+        self.superuser = get_user_model().objects.create_superuser(
+            username='suser',
+            email='suser@s.com',
+            password='spassword'
+        )
+        self.assertEqual(self.superuser.username, 'suser')
+        self.assertEqual(self.superuser.email, 'suser@s.com')
+        self.assertTrue(self.superuser.check_password('spassword'))
+        self.assertTrue(self.superuser.is_staff)
+        self.assertTrue(self.superuser.is_superuser)
+    
+class ProfileModelTest(TestCase):
+    
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            password='testpassword'
+        )
+        self.profile = Profile.objects.create(
+            user=self.user,
+            date_of_birth='2001-12-12',
+            fav_author = 'Archer'
+        )
+    
+    def test_profile_creation(self):
+        self.assertEqual(self.profile.user, self.user)
+        self.assertEqual(self.profile.date_of_birth, '2001-12-12')
+        self.assertEqual(self.profile.fav_author, 'Archer')
+    
+    def test_profile_str_representation(self):
+        expected_str = "testuser"
+        self.assertEqual(str(self.profile), expected_str)
+    
+    def test_get_absolute_url_method(self):
+        # Test the get_absolute_url method of Profile
+        expected_url = '/accounts/profile/{}/'.format(self.profile.pk)
+        self.assertEqual(self.profile.get_absolute_url(), expected_url)
--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,50 @@
-from django.test import TestCase
+from django.test import Client, TestCase
+from django.urls import reverse
+from .models import Book
 
-# Create your tests here.
+class BookTests(TestCase):
+    
+    def setUp(self):
+        self.book = Book.objects.create(
+            title='Django 4 By Example',
+            author='Anthony Mele',
+            price='25.00',
+            date_publication='2022-08-01'
+        )
+    
+    def test_book_listing(self):
+        self.assertEqual(f'{self.book.title}', 'Django 4 By Example')
+        self.assertEqual(f'{self.book.author}', 'Anthony Mele')
+        self.assertEqual(f'{self.book.price}', '25.00')
+        self.assertEqual(f'{self.book.date_publication}', '2022-08-01')
+    
+    def test_book_list_view(self):
+        response = self.client.get(reverse('book_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Django 4 By Example')
+        self.assertTemplateUsed(response, 'books/book_list.html')
+    
+    def test_book_detail_view(self):
+        response = self.client.get(self.book.get_absolute_url())
+        no_response = self.client.get('/books/12345/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, 'Django 4 By Example')
+        self.assertTemplateUsed(response, 'books/book_detail.html')
+    
+    def test_search_results_view(self):
+        response = self.client.get(reverse('search_results'), {'q': 'Django'})
+        no_result = self.client.get(reverse('search_results'), {'q': 'Software'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Django')
+        self.assertEqual(no_result.status_code, 200)
+        self.assertNotContains(no_result, 'Software')
+    
+    def test_book_str_representation(self):
+        expected_str = "Django 4 By Example"
+        self.assertEqual(str(self.book), expected_str)
+    
+    def test_get_absolute_url_method(self):
+        # Test the get_absolute_url method of Profile
+        expected_url = '/books/{}/'.format(self.book.pk)
+        self.assertEqual(self.book.get_absolute_url(), expected_url)
